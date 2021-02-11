4e8d3b66be669706eafd69be8764946c11fea579
Davor Rilko <drilko@span.eu>
2021-02-11 14:52:01 +0100

    initial

---
+++ EfCoreMigrations/AppContext.cs
@@ -0,0 +1,27 @@
+﻿using EfCoreMigrations.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EfCoreMigrations
+{
+    public class AppContext: DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>().ToTable("Course");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+            modelBuilder.Entity<Student>().ToTable("Student");
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=.;Database=School;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+    }
+}
